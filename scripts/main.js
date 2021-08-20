be831bb484f46dd40b03d58400e1994d4c16dfc2
Alexand Bubyr <alexandr.bybur@gmail.com>
2021-08-20 22:21:05 +0300

    add: modal window

--- scripts/main.js
+++ scripts/main.js
@@ -3,8 +3,14 @@ $(document).ready(function () {
     let floorPath = $('.home-image path');
     let counterUp = $('.counter-up');
     let counterDown = $('.counter-down');
+    let modal = $('.modal');
+    let modalCloseButton = $('.modal-close-button');
+    let viewFlatsButton = $('.view-flats');
     let clearFloor = function () {
         floorPath.removeClass('current-floor');
+    };
+    let toggleModal = function () {
+        modal.toggleClass('is-open');
     };
     floorPath.on('mouseover', function () {
         clearFloor();
@@ -29,4 +35,7 @@ $(document).ready(function () {
             $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
         }
     });
+    floorPath.on('click', toggleModal);
+    modalCloseButton.on('click', toggleModal);
+    viewFlatsButton.on('click', toggleModal);
 });
